700622c599ba2429fec3425789876b0892ba4dfa
Emilio Rodriguez <emiliorodriguez@gmail.com>
2017-05-19 12:40:17 +0200

    added chapter2 - RSS Reader

---
+++ Chapter02/src/screens/FeedsList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { NavigationActions } from 'react-navigation';
+import { Container, Content, List, ListItem, Text, Icon, Button } from 'native-base';
+import { observer } from 'mobx-react/native';
+import { selectFeed, removeFeed } from '../actions';
+
+@observer
+export default class FeedsList extends React.Component {
+  static navigationOptions = (props) => ({
+    title: 'My Feeds',
+    headerRight: <Button 
+      transparent
+      onPress={ () => props.navigation.navigate('AddFeed') }>
+        <Icon name='add'/>
+      </Button>
+  })
+
+  _handleFeedPress(feed) {
+    selectFeed(feed);
+    this.props.navigation.navigate('FeedDetail', 
+      { 
+        removeFeed: () => { removeFeed(feed.url); }
+      }
+    );
+  }
+
+  render() {
+    const { feeds } = this.props.screenProps.store;
+    return (
+      <Container>
+          <Content>
+              <List>
+                  {
+                    feeds && feeds.map((f, i) => {
+                      return (
+                        <ListItem key={i} onPress={this._handleFeedPress.bind(this, f)}>
+                          <Text>{f.title}</Text>
+                        </ListItem>
+                      );
+                    })
+                  }
+              </List>
+          </Content>
+      </Container>
+    )
+  }
+}
